38deb03843f0d69dd436626282dcfc854cec70f2
ThomasPicot <71181911+ThomasPicot@users.noreply.github.com>
2021-05-25 15:21:18 +0200

    Update classspectrum.py

--- Classes/classspectrum.py
+++ Classes/classspectrum.py
@@ -3,32 +3,26 @@ import scipy.constants as cst
 from scipy.special import wofz
 
 
-# voigt function
-def V(z):
-    return 1j*wofz(1j*z)
+class SpectrumRubidiumD2Line:
+    def __init__(self, gamma: float = 3.81138e7, k: float = 2 * np.pi / 7.807864080702083e-7,
+                 d: float = 4.4003140382156403e-29, M: float = 1.4099931997e-25, beta: float = 1.03e-13):
+        """
 
-
-def Ndensity(temp, deg):
-    N = (133.323 * 10 ** (15.88253 - (4529.635 / temp) + 0.00058663 * temp - 2.99138 * np.log10(temp))) / (
-                cst.Boltzmann * temp)
-    return N/deg
-
-
-# change the temperature parameter in the function
-def varsigma(temp):
-    sigma = 2 * np.pi * np.sqrt((2 * cst.k * temp) / (1.4099931997e-25 * 7.807864080702083e-7 ** 2))
-    return sigma
-
-
-class DisplaySpectrum:
-    def __init__(self, gamma=0.5 * 3.81138e7, k=2*np.pi/7.807864080702083e-7, v=2e3, m=1.4099931997e-25):
+        Parameters
+        ----------
+        gamma : decay time for the stage |e> -> |g>
+        k : laser wave number
+        d : electric dipole
+        M : atomic mass
+        beta : [SI] impact self broadening
+        """
         self.h = cst.hbar
         self.eps_zero = cst.epsilon_0
+        self.beta = beta
         self.gamma = gamma
         self.k = k
-        self.d = 3.24627e-29
-        self.v = v
-        self.m = m
+        self.d = d
+        self.M = M
 
         # Rb 87
 
@@ -54,35 +48,98 @@ class DisplaySpectrum:
         self.w_jjb = 1664.714e6
         self.w_jjjb = 1728.134e6
 
-    def varomega(self):
-        omega = 2*np.pi*np.linspace(-5e9, 5e9, 3000)     # useless to touch it
-        return omega
+    @staticmethod
+    def voigtProfile(z: float) -> float:
+        """
+        function that has a gaussian profile and that takes the effect of doppler broadening.
+        :param z: function you want to apply the voigt profile
+        :return: non-complex number voigt profile
+        """
+        return 1j * wofz(1j * z)
 
-    def fracRb(self, frac, temp, deg):
-        N = frac*Ndensity(temp, deg)
+    @staticmethod
+    def Ndensity(temp: int, deg: int) -> float:
+        """
+        calculates the atomic density, depending on the isotope and temperature.
+        :param temp: temperature T[K] (local var)
+        :param deg: degeneration degree, depending of the isotope. 8 for 87 and 12 for 85
+        :return: density N depending of the degeneration
+        """
+        N = (133.323 * 10 ** (15.88253 - (4529.635 / temp) + 0.00058663 * temp - 2.99138 * np.log10(temp))) / (
+                cst.Boltzmann * temp)
+        return N / deg
+
+    @staticmethod
+    def sigma(temp: int) -> float:
+        """
+        width of the velocity distribution
+        :param temp: temperature T[K] (local var)
+        :return: sigma
+        """
+        sigma: float = 2 * np.pi * np.sqrt((2 * cst.k * temp) / (1.4099931997e-25 * 7.807864080702083e-7 ** 2))
+        return sigma
+
+    def detuning(self) -> np.array([float]):
+        """
+        variable
+        :return: detuning array
+        """
+        detuning: array = np.linspace(-7e9, 7e9, 2000)  # useless to touch it
+        return detuning
+
+    def atomNumber(self, frac: int, temp: int, deg: int) -> float:
+        """
+        :param frac:
+        :param temp:
+        :param deg:
+        :return: N the mean atom number contained in m^3
+        """
+        N = frac * self.Ndensity(temp, deg)
         return N
 
-    def absorption(self, C_f, frac, temp, detuning, deg):
-        N = self.fracRb(frac, temp, deg)
-        delta = 2*np.pi*(self.varomega() - detuning)
-        voigt_arg = (self.gamma - 1j * delta) / varsigma(temp)
-        absorption = C_f*(N * (self.d ** 2) * np.sqrt(np.pi) / (self.h * self.eps_zero *varsigma(temp))) * V(
-            voigt_arg).imag
-        return absorption
+    def susceptibility(self, C_f: float, frac: int, temp: int, transition_frequency: float,
+                       deg: int) -> np.array([float]):
+        """
 
-    def alpha(self, C_f, frac, temp, detuning, deg):
-        alpha = self.k*self.absorption(C_f, frac, temp, detuning, deg)
+        :param C_f: C-f^2 transition coefficient, depending on the quantum number F and isotope
+        :param frac: % of Rb 87 in the cell. 0 < frac < 100 -> 0 < frac < 1
+        :param temp: temperature T[K] (local var)
+        :param transition_frequency: possible values are instance variables in init that depend of the quantum number F.
+        :param deg: degeneration degree, depending of the isotope. 8 for 87 and 12 for 85
+        :return: absorption coefficient
+        """
+        N: float = self.atomNumber(frac, temp, deg)
+        delta: np.array([int]) = 2 * np.pi * (self.detuning() - transition_frequency)
+        voigt_arg = (0.5*(self.gamma+self.beta*self.Ndensity(temp=temp, deg=deg)) - 1j * delta) / self.sigma(temp)
+        suscepti = C_f * (N * (self.d ** 2) * np.sqrt(np.pi) / (self.h * self.eps_zero * self.sigma(temp))) * \
+                   self.voigtProfile(voigt_arg).imag
+        return suscepti
+
+    def alpha(self, C_f, frac, temp, transition_frequency, deg) -> np.array([float]):
+        """
+        absorption coefficient alpha
+        :param C_f:
+        :param frac:
+        :param temp:
+        :param transition_frequency:
+        :param deg:
+        :return:
+        """
+        alpha = self.k * self.susceptibility(C_f, frac, temp, transition_frequency, deg)
         return alpha
 
-    def transmission(self, frac=None, temp=300, long=0.075):
-        sum_alpha = self.alpha(10/81, 1-(frac/100), temp, self.w_j, 12) + self.alpha(35/81, 1-(frac/100),
-                                                                                     temp, self.w_jj, 12) + \
-                    self.alpha(1, 1-(frac/100), temp, self.w_jjj, 12) + self.alpha(1/3, 1-(frac/100), temp,
-                                                                                   self.w_jb, 12) + \
-                    self.alpha(35/81, 1-(frac/100), temp, self.w_jjb, 12) + self.alpha(28/81, 1-(frac/100), temp,
-                                                                                       self.w_jjjb, 12) +\
-                    self.alpha(1/18, frac/100, temp, self.w_i, 8) + self.alpha(5/18, frac/100, temp, self.w_ii, 8) +\
-                    self.alpha(7/9, frac/100, temp, self.w_iii, 8) + self.alpha(1/9, frac/100, temp, self.w_ib, 8) +\
-                    self.alpha(5/18, frac/100, temp, self.w_iib, 8) + self.alpha(5/18, frac/100, temp, self.w_iiib, 8)
+    def transmission(self, frac=None, temp: int = 300, long: float = 0.075) -> np.array([float]):
+        sum_alpha = self.alpha(10 / 81, 1 - (frac / 100), temp, self.w_j, 12) + \
+                    self.alpha(35 / 81, 1 - (frac / 100), temp, self.w_jj, 12) + \
+                    self.alpha(1, 1 - (frac / 100), temp, self.w_jjj, 12) + \
+                    self.alpha(1 / 3, 1 - (frac / 100), temp, self.w_jb, 12) + \
+                    self.alpha(35 / 81, 1 - (frac / 100), temp, self.w_jjb, 12) + \
+                    self.alpha(28 / 81, 1 - (frac / 100), temp, self.w_jjjb, 12) + \
+                    self.alpha(1 / 18, frac / 100, temp, self.w_i, 8) + \
+                    self.alpha(5 / 18, frac / 100, temp, self.w_ii, 8) + \
+                    self.alpha(7 / 9, frac / 100, temp, self.w_iii, 8) + \
+                    self.alpha(1 / 9, frac / 100, temp, self.w_ib, 8) + \
+                    self.alpha(5 / 18, frac / 100, temp, self.w_iib, 8) + \
+                    self.alpha(5 / 18, frac / 100, temp, self.w_iiib, 8)
         transmi = np.exp(-sum_alpha * long)
         return transmi
